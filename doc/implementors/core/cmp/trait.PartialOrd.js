46acb51d1a435c634b6c9a5bcad2bc8bd9b47566
Andrew Chin <achin@eminence32.net>
2014-08-03 19:22:54 -0400

    update docs

---
+++ doc/implementors/core/cmp/trait.PartialOrd.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['git2'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/master/core/cmp/trait.PartialOrd.html' title='core::cmp::PartialOrd'>PartialOrd</a> for <a class='struct' href='git2/git2/struct.CapabilityFlags.html' title='git2::git2::CapabilityFlags'>CapabilityFlags</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ doc/implementors/core/ops/trait.BitAnd.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['git2'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/master/core/ops/trait.BitAnd.html' title='core::ops::BitAnd'>BitAnd</a>&lt;<a class='struct' href='git2/git2/struct.CapabilityFlags.html' title='git2::git2::CapabilityFlags'>CapabilityFlags</a>, <a class='struct' href='git2/git2/struct.CapabilityFlags.html' title='git2::git2::CapabilityFlags'>CapabilityFlags</a>&gt; for <a class='struct' href='git2/git2/struct.CapabilityFlags.html' title='git2::git2::CapabilityFlags'>CapabilityFlags</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
