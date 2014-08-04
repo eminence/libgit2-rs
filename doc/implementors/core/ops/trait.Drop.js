7fe50162e7526f309ac6accbb09e71a186acd8c6
Andrew Chin <achin@eminence32.net>
2014-08-03 23:10:19 -0400

    Updated docs

--- doc/implementors/core/ops/trait.Drop.js
+++ doc/implementors/core/ops/trait.Drop.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['git2'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/master/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='git2/git2/config/struct.GitConfigIterator.html' title='git2::git2::config::GitConfigIterator'>GitConfigIterator</a>",];
+implementors['git2'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/master/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='git2/git2/config/struct.GitConfigIterator.html' title='git2::git2::config::GitConfigIterator'>GitConfigIterator</a>","<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/master/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='git2/git2/branch/struct.GitBranchIterator.html' title='git2::git2::branch::GitBranchIterator'>GitBranchIterator</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
